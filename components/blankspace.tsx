df7c9cdd34b6427e65f500620663735ee4e64e4d
Anmoldeep1509 <anmoldeep1509@gmail.com>
2022-02-09 01:40:30 +0530

    created about page, blankspace component

--- components/blankspace.tsx
+++ components/blankspace.tsx
@@ -1,10 +1,21 @@
 const BlankSpace = () => {
-    return (
-      <div className="container mx-auto lg:flex lg:flex-col">
+  return (
+    <div className="container py-4 mx-auto text-center text-white ">
+      <p className="py-4 text-3xl">
+        Seems like you found a baby space...
         
-      </div>
-    );
-  };
-  
-  export default BlankSpace;
-  
+      </p>
+        <p className="my-6 mb-12 text-lg">We are working on this area, <br />Check back later to see more!</p>
+
+        <a
+            className="px-6 py-3 mb-8 font-semibold text-white bg-red-600 rounded-full "
+            href="../"
+          >
+            Go Home
+           
+          </a>
+    </div>
+  );
+};
+
+export default BlankSpace;
