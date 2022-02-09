6795b33dfda207551a5e5be73cfe4b6c791eea88
seebham <shubhamisdev@gmail.com>
2022-02-09 16:25:40 +0530

    feat: :sparkles: OG and Meta Tags added

--- pages/_document.js
+++ pages/_document.js
@@ -5,10 +5,19 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
+          <meta
+            name="description"
+            content="TEDxYCCE, x = independently organised TED event. Ideas worth spreading. This April, at YCCE, Nagpur."
+          />
+          <meta
+            name="keywords"
+            content="TEDx, YCCE, Yeshwantrao Chavan College of Engineering, Hingna, Nagpur, event, talks"
+          />
           <link
             rel="stylesheet"
             href="https://cdn.jsdelivr.net/npm/@xz/fonts@1/serve/plus-jakarta-display.min.css"
           />
+          <link rel="icon" href="/favicon.ico" />
         </Head>
         <body>
           <Main />
