dfbe34ba0daea127bbd5f842a22d46a826bec24d
Destro <swanandev@gmail.com>
2022-02-10 19:16:51 +0530

    Added Contact Details

--- components/contactUs.tsx
+++ components/contactUs.tsx
@@ -1,12 +1,100 @@
-import React from 'react'
+import React from "react";
 
 const ContactUs = () => {
   return (
     <div className="container flex flex-row justify-center gap-6 mx-auto text-white">
-        <div>Contact Form - Anmoldeep</div>
-        <div>Contact Info and loc card - destro</div>
-    </div>
-  )
-}
+      <div>Contact Form - Anmoldeep</div>
+      <div className="flex flex-col text-red-700">
+        <h2 className="text-3xl font-semibold pb-9 text-white">
+          Where can you find us
+        </h2>
+        <div className="py-2 flex gap-2">
+          <svg
+            className="h-7 w-7 "
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+          >
+            <path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z" />
+            <path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z" />
+          </svg>
 
-export default ContactUs
+          <a
+            href="mailto:organizer@tedxycce.in"
+            target="_blank"
+            rel="noreferrer"
+            className="text-white"
+          >
+            organizer@tedxycce.in
+          </a>
+        </div>
+        <div className="py-2 flex gap-2">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-7 w-7 "
+            fill="currentColor"
+            viewBox="0 0 24 24"
+            stroke="none"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z"
+            />
+          </svg>
+          <a
+            href="tel:+91 9999999999"
+            target="_blank"
+            rel="noreferrer"
+            className="text-white"
+          >
+            +91 86685 50779
+          </a>
+          <a
+            target="_blank"
+            rel="noreferrer"
+            className="text-white"
+            href="https://api.whatsapp.com/send?phone=918668550779&text=Hi%20TEDxYCCE,%20Let%27s%20have%20a%20chat?"
+          >
+            (Same for Whatsapp)
+          </a>
+        </div>
+        <div className="py-2 flex gap-2">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-7 w-7"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+          >
+            <path
+              fillRule="evenodd"
+              d="M10 18a8 8 0 100-16 8 8 0 000 16zM4.332 8.027a6.012 6.012 0 011.912-2.706C6.512 5.73 6.974 6 7.5 6A1.5 1.5 0 019 7.5V8a2 2 0 004 0 2 2 0 011.523-1.943A5.977 5.977 0 0116 10c0 .34-.028.675-.083 1H15a2 2 0 00-2 2v2.197A5.973 5.973 0 0110 16v-2a2 2 0 00-2-2 2 2 0 01-2-2 2 2 0 00-1.668-1.973z"
+              clipRule="evenodd"
+            />
+          </svg>
+
+          <a
+            href="https://ycce.edu"
+            target="_blank"
+            rel="noreferrer"
+            className="text-white"
+          >
+            Yeshwantrao Chavan College of Engineering
+          </a>
+        </div>
+
+        <iframe
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3722.4356442935323!2d78.9785723!3d21.0951911!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bd4952117eaac51%3A0x6d8277793eb63d6a!2sYeshwantrao%20Chavan%20College%20of%20Engineering%20(YCCE)%2C%20Nagpur!5e0!3m2!1sen!2sin!4v1644498444516!5m2!1sen!2sin"
+          width="600"
+          height="450"
+          className="border-0 py-2"
+          allowFullScreen={false}
+          loading="lazy"
+        ></iframe>
+      </div>
+    </div>
+  );
+};
+
+export default ContactUs;
