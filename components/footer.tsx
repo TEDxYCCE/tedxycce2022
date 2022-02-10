ed982cd3451334a9e218ac51909455df00c41ac3
Anmoldeep1509 <anmoldeep1509@gmail.com>
2022-02-10 20:27:56 +0530

    made lg footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,101 +1,105 @@
 const Footer = () => {
   return (
-    <footer className="container mx-auto flex flex-col items-center p-6 pt-8 text-white bg-black">
-      <ul id="socials" className="inline-flex gap-4 p-2 text-red-700">
-        <li>
-          <a
-            href="https://www.facebook.com/tedxycceofficial"
-            rel="noopener noreferrer"
-            target="_blank"
-          >
-            <svg
-              viewBox="0 0 24 24"
-              width="24"
-              height="24"
-              stroke="currentColor"
-              strokeWidth="2"
-              fill="none"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              className="w-10 h-10"
+    <footer className="container flex flex-col items-center p-6 pt-8 mx-auto text-white bg-black lg:flex-row lg:flex-inverse lg:justify-between lg:items-center">
+      <div>
+        <ul id="socials" className="inline-flex gap-4 p-2 mt-10 text-red-700">
+          <li>
+            <a
+              href="https://www.facebook.com/tedxycceofficial"
+              rel="noopener noreferrer"
+              target="_blank"
             >
-              <path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z"></path>
-            </svg>
-          </a>
-        </li>
-        <li>
-          <a
-            href="https://www.instagram.com/tedxycce/"
-            rel="noopener noreferrer"
-            target="_blank"
-          >
-            <svg
-              viewBox="0 0 24 24"
-              width="24"
-              height="24"
-              stroke="currentColor"
-              strokeWidth="2"
-              fill="none"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              className="w-10 h-10"
+              <svg
+                viewBox="0 0 24 24"
+                width="24"
+                height="24"
+                stroke="currentColor"
+                strokeWidth="1.5"
+                fill="none"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                className="w-10 h-10"
+              >
+                <path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z"></path>
+              </svg>
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.instagram.com/tedxycce/"
+              rel="noopener noreferrer"
+              target="_blank"
             >
-              <rect x="2" y="2" width="20" height="20" rx="5" ry="5"></rect>
-              <path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z"></path>
-              <line x1="17.5" y1="6.5" x2="17.51" y2="6.5"></line>
-            </svg>
-          </a>
-        </li>
+              <svg
+                viewBox="0 0 24 24"
+                width="24"
+                height="24"
+                stroke="currentColor"
+                strokeWidth="1.5"
+                fill="none"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                className="w-10 h-10"
+              >
+                <rect x="2" y="2" width="20" height="20" rx="5" ry="5"></rect>
+                <path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z"></path>
+                <line x1="17.5" y1="6.5" x2="17.51" y2="6.5"></line>
+              </svg>
+            </a>
+          </li>
 
-        <li>
-          <a
-            href="https://twitter.com/tedxycce"
-            rel="noopener noreferrer"
-            target="_blank"
-          >
-            <svg
-              viewBox="0 0 24 24"
-              width="24"
-              height="24"
-              stroke="currentColor"
-              strokeWidth="2"
-              fill="none"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              className="w-10 h-10"
+          <li>
+            <a
+              href="https://twitter.com/tedxycce"
+              rel="noopener noreferrer"
+              target="_blank"
             >
-              <path d="M23 3a10.9 10.9 0 0 1-3.14 1.53 4.48 4.48 0 0 0-7.86 3v1A10.66 10.66 0 0 1 3 4s-4 9 5 13a11.64 11.64 0 0 1-7 2c9 5 20 0 20-11.5a4.5 4.5 0 0 0-.08-.83A7.72 7.72 0 0 0 23 3z"></path>
-            </svg>
-          </a>
-        </li>
-        <li>
-          <a
-            href="https://www.linkedin.com/company/tedxycce/"
-            rel="noopener noreferrer"
-            target="_blank"
-          >
-            <svg
-              viewBox="0 0 24 24"
-              width="24"
-              height="24"
-              stroke="currentColor"
-              strokeWidth="2"
-              fill="none"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              className="w-10 h-10"
+              <svg
+                viewBox="0 0 24 24"
+                width="24"
+                height="24"
+                stroke="currentColor"
+                strokeWidth="1.5"
+                fill="none"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                className="w-10 h-10"
+              >
+                <path d="M23 3a10.9 10.9 0 0 1-3.14 1.53 4.48 4.48 0 0 0-7.86 3v1A10.66 10.66 0 0 1 3 4s-4 9 5 13a11.64 11.64 0 0 1-7 2c9 5 20 0 20-11.5a4.5 4.5 0 0 0-.08-.83A7.72 7.72 0 0 0 23 3z"></path>
+              </svg>
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.linkedin.com/company/tedxycce/"
+              rel="noopener noreferrer"
+              target="_blank"
             >
-              <path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z"></path>
-              <rect x="2" y="9" width="4" height="12"></rect>
-              <circle cx="4" cy="4" r="2"></circle>
-            </svg>
-          </a>
-        </li>
-      </ul>
-      <p className="mt-2 text-xl">Copyright © 2022 TEDxYCCE</p>
-      <p className="my-2 text-gray-400 text-justify">
-        This independent TEDx event is operated under license from TED.
-      </p>
+              <svg
+                viewBox="0 0 24 24"
+                width="24"
+                height="24"
+                stroke="currentColor"
+                strokeWidth="1.5"
+                fill="none"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                className="w-10 h-10"
+              >
+                <path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z"></path>
+                <rect x="2" y="9" width="4" height="12"></rect>
+                <circle cx="4" cy="4" r="2"></circle>
+              </svg>
+            </a>
+          </li>
+        </ul>
+      </div>
+      <div>
+        <p className="mt-2 text-xl">Copyright © 2022 TEDxYCCE</p>
+        <p className="my-2 text-justify text-gray-400">
+          This independent TEDx event is operated under license from TED.
+        </p>
+      </div>
     </footer>
   );
 };
