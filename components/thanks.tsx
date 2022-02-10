36e4908aaf70fd66ca60f75071786272601adc74
Anmoldeep1509 <anmoldeep1509@gmail.com>
2022-02-10 19:47:53 +0530

    Added: Thanks page

---
+++ components/thanks.tsx
@@ -0,0 +1,9 @@
+const Thanks = () => (
+    <div className="container h-40 py-10 mx-auto text-xl text-center text-white border-2 border-red-800 ">
+        Thanks for your message, we will reply to you shortly!
+
+
+    </div>
+)
+
+export default Thanks;
