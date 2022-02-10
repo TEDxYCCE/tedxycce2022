5f33fb7087e234c2b54232ab7fbb92d198af4eac
Anmoldeep1509 <anmoldeep1509@gmail.com>
2022-02-10 18:28:15 +0530

    initialize contact section

---
+++ components/contactUs.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const ContactUs = () => {
+  return (
+    <div className="container flex flex-row justify-center gap-6 mx-auto text-white">
+        <div>Contact Form - Anmoldeep</div>
+        <div>Contact Info and loc card - destro</div>
+    </div>
+  )
+}
+
+export default ContactUs
