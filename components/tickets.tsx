d5e2dcf2bb546adf3042855cef1c7fc142afd272
Shubham Lingayat <shubham@raftlabs.co>
2022-04-28 18:19:06 +0530

    refactor: cleaned tickets.tsx code

--- components/tickets.tsx
+++ components/tickets.tsx
@@ -1,28 +1,13 @@
-import Image from "next/image";
 import Button from "./button";
 import SectionHeading from "./sectionHeading";
 
-const stairsClasses =
-  "flex border-l-4 border-t-4 md:border-l-8 md:border-t-8 border-red-800 pl-1 py-1 gap-1";
-
 const Tickets = () => {
   return (
-    <section className="">
+    <section id="register" className="">
       <div className="container px-6 py-10 mx-auto lg:py-24 lg:flex lg:flex-col lg:px-32 ">
         <SectionHeading title="Register for TEDxYCCE" />
         <div className="container flex flex-col justify-center mx-auto md:flex-row">
-          {/* <div className="w-5/6 mb-10 rounded lg:max-w-lg lg:w-full md:w-1/2 md:mb-0">
-            <Image
-              className="object-cover object-center mx-auto"
-              alt="hero"
-              width="720px"
-              src="https://dummyimage.com/720x600"
-              height="600px"
-            />
-          </div> */}
-          {/* <div className="flex flex-col items-center text-center lg:flex-grow md:w-5/6 lg:pl-24 md:p-16 md:items-start md:text-left"> */}
           <div className="flex flex-col items-center text-center lg:flex-grow md:w-5/6 md:px-10 ">
-            {/* <h1 className="mb-4 text-3xl font-medium text-gray-900 title-font sm:text-4xl"> */}
             <h1 className="mb-2 text-xl font-medium leading-5 tracking-wide sm:text-4xl ">
               Early Bird Ticket: Rs. 1200/-
               {/* <br className="hidden lg:inline-block" />
@@ -63,20 +48,6 @@ const Tickets = () => {
             </div>
           </div>
         </div>
-        {/* description */}
-        {/* <div className="text-2xl text-center text-gray-300">
-          Under the stairs
-        </div>
-        <h3 className="mt-2 text-xl font-semibold text-center">
-          TEDxYCCE 2021
-        </h3>
-        <div className="py-4 mt-2 text-center text-gray-200 lg:px-24 xl:px-52">
-          To unwrap the invisible foundation of the stairs and explore its
-          incredible power, TEDxYCCE 2021 came up with the broad theme
-          &quot;Under the Stairs&quot; which is an idea to uncover the hidden
-          treasure below the Stairs.
-        </div> */}
-        {/* Button to TEDxYCCE 2021 Website */}
       </div>
     </section>
   );
